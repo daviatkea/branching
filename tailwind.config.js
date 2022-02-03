994f3a1cc163826e1df142f82d6403b68d740a06
Dannie Vinther <davi@kea.dk>
2022-02-03 13:41:20 +0100

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#5000ca",
+    },
+    fontFamily: {
+      poppins: "poppins, system-ui",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
